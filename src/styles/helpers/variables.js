d5b8003197ca3c02be2ed4a15a75c10d59d703e8
Tiago <tiagojacintodev@gmail.com>
2022-08-24 10:47:01 +0200

    Define Style Variables

---
+++ src/styles/helpers/variables.js
@@ -0,0 +1,36 @@
+export const variable = {
+  fontSize: {
+    small: '0.6rem',
+    medium: '1rem',
+    large: '1.5rem',
+    larger: '2.5rem',
+  },
+  fontWeight: {
+    regular: 400,
+    semiBold: 500,
+    bold: 700,
+  },
+  fontFamily: {
+    logo: 'ZCOOL KuaiLe',
+    default: 'Inter',
+  },
+  size: {
+    small: '60px',
+    medium: '110px',
+    large: '150px',
+  },
+  colors: {
+    accent: '#FE5107',
+    black: 'black',
+    white: 'white',
+    lightGray: '#C4C4C4',
+    lightDark: '#0A0A0A',
+  },
+  deviceSize: {
+    mobile: 640,
+    tablet: 768,
+    laptop: 1024,
+    desktop: 1280,
+    widescreen: 1536,
+  },
+};
